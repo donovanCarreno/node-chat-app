15995a7274c049fc9b660a022ec9ced88639b416
donovanCarreno <donovan_carreno@yahoo.com>
2017-09-16 13:37:45 -0500

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 const socket = io()
 
+function scrollToBottom() {
+  // Selectors
+  var messages = $('#messages')
+  var newMessage = messages.children('li:last-child')
+
+  // Heights
+  var clientHeight = messages.prop('clientHeight')
+  var scrollTop = messages.prop('scrollTop')
+  var scrollHeight = messages.prop('scrollHeight')
+  var newMessageHeight = newMessage.innerHeight()
+  var lastMessageHeight = newMessage.prev().innerHeight()
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function() {
   console.log('connected to server')
 })
@@ -18,6 +35,7 @@ socket.on('newMessage', function(msg) {
   })
 
   $('#messages').append(html)
+  scrollToBottom()
 })
 
 socket.on('newLocationMessage', function(msg) {
@@ -30,6 +48,7 @@ socket.on('newLocationMessage', function(msg) {
   })
 
   $('#messages').append(html)
+  scrollToBottom()
 })
 
 $('#message-form').on('submit', function(e) {
